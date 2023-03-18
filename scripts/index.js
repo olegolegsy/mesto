a4dd57288ee7ac7a57e048648bd315caa9641111
Oleg Sidorov <ya.sidorovoleg8700@yandex.ru>
2023-03-18 12:57:39 +0300

    правки 1\2

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-job');
+let formElement = document.querySelector('.popup__save-btn');
 
-function closeForm (evt) {
-  evt.preventDefault();
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 
-  popup.classList.add('popup_cls');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__job');
+
+let formCls = document.querySelector('.popup__cls-btn');
+let popup = document.querySelector('.popup');
+let profileEdit = document.querySelector('.profile__edit-btn');
+
+function closeForm () {
+  popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
@@ -14,29 +20,17 @@ function handleFormSubmit (evt) {
   let name = nameInput.value;
   let job = jobInput.value;
 
-  let profileName = document.querySelector('.profile__title');
-  let profileJob = document.querySelector('.profile__job');
-
   profileName.textContent = name;
   profileJob.textContent = job;
 
-  popup.classList.add('popup_cls');
+  popup.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+function openForm () {
+  popup.classList.add('popup_opened')
+}
 
-let formCls = document.querySelector('.popup__cls-btn');
-let popup = document.querySelector('.popup');
-
+formElement.addEventListener('click', handleFormSubmit);
 formCls.addEventListener('click', closeForm);
-
-let profileEdit = document.querySelector('.profile__edit-btn');
-
-function openForm (evt) {
-  evt.preventDefault();
-
-  popup.classList.remove('popup_cls')
-}
-
 profileEdit.addEventListener('click', openForm);
 
